1ebde1681eece42f423345abf0b24999cb2f7692
brofu <brofu.talk@gmail.com>
2023-10-18 19:54:01 +0800

    add problem 206 revert list and the extension problems

---
+++ list/list_lc206.go
@@ -0,0 +1,114 @@
+package list
+
+/*
+Problem 206
+Key Point
+Versions
+	*	Iterate version 1. Bad performance
+	*	Iterate version 2. Multiple pointer and the controlling logic
+	*	Recursive version 3
+	*	Recursive version 4. Key point is, how to use 1 pointer, instead of 2?
+
+Extensions
+	*	Reverse the first N nodes.
+		*	return 2 parameters or use global var
+		*	The edges cases
+			*	n > list length
+*/
+
+// Iterate verion 1.
+func reverseList(head *ListNode) *ListNode {
+
+	nodes := []*ListNode{}
+	dummy := &ListNode{
+		-1,
+		nil,
+	}
+	p := dummy
+
+	for head != nil {
+		nodes = append(nodes, head)
+		head = head.Next
+	}
+
+	for index := len(nodes) - 1; index >= 0; index -= 1 {
+		p.Next = nodes[index]
+		p = p.Next
+	}
+
+	p.Next = nil
+
+	return dummy.Next
+}
+
+// Iterate verion 2.
+func reverseListV2(head *ListNode) *ListNode {
+	var before, after *ListNode
+	for head != nil {
+		after = head.Next
+		head.Next = before
+
+		before = head
+		head = after
+	}
+	return before
+}
+
+// Recursive version 3
+func reverseListV3(head *ListNode) *ListNode {
+
+	if head != nil && head.Next != nil {
+		newHead, _ := reverseListHelper(head, head.Next)
+		return newHead
+	}
+
+	return head
+}
+
+func reverseListHelper(current, next *ListNode) (*ListNode, *ListNode) {
+
+	current.Next = nil
+	if next.Next != nil {
+		head, tail := reverseListHelper(next, next.Next)
+		tail.Next = current
+		return head, current
+	}
+	next.Next = current
+	return next, current
+}
+
+// Recursive version 3, better implementation. More briefing.
+
+func reverseListV4(head *ListNode) *ListNode {
+
+	if head == nil || head.Next == nil {
+		return head
+	}
+
+	last := reverseListV4(head.Next)
+	head.Next.Next = head
+	head.Next = nil
+	return last
+}
+
+// reverse version
+func reverseListFirstN(head *ListNode, n int) *ListNode {
+	last, _ := reverseListFirstNHelper(head, n)
+	return last
+}
+
+func reverseListFirstNHelper(head *ListNode, n int) (*ListNode, *ListNode) {
+	if head == nil {
+		return head, nil
+	}
+
+	if n <= 1 || head.Next == nil {
+		return head, head.Next
+	}
+
+	last, tail := reverseListFirstNHelper(head.Next, n-1)
+	head.Next.Next = head
+	head.Next = tail
+
+	return last, tail
+}
---
+++ list/list_lc206_test.go
@@ -0,0 +1,223 @@
+package list
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_reverseList(t *testing.T) {
+	type args struct {
+		head []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			name: "lc case 1",
+			args: args{
+				head: []int{1, 2, 3, 4, 5},
+			},
+			want: []int{5, 4, 3, 2, 1},
+		},
+		{
+			name: "lc case 2",
+			args: args{
+				head: []int{1, 2},
+			},
+			want: []int{2, 1},
+		},
+		{
+			name: "lc case 3",
+			args: args{
+				head: []int{},
+			},
+			want: []int{},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			list := ConstructListNodeFromSlice(tt.args.head)
+			got := reverseList(list)
+			want := ConstructListNodeFromSlice(tt.want)
+			if !reflect.DeepEqual(got, want) {
+				t.Errorf("reverseList() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_reverseListV2(t *testing.T) {
+	type args struct {
+		head []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			name: "lc case 1",
+			args: args{
+				head: []int{1, 2, 3, 4, 5},
+			},
+			want: []int{5, 4, 3, 2, 1},
+		},
+		{
+			name: "lc case 2",
+			args: args{
+				head: []int{1, 2},
+			},
+			want: []int{2, 1},
+		},
+		{
+			name: "lc case 3",
+			args: args{
+				head: []int{},
+			},
+			want: []int{},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			list := ConstructListNodeFromSlice(tt.args.head)
+			got := reverseListV2(list)
+			want := ConstructListNodeFromSlice(tt.want)
+			if !reflect.DeepEqual(got, want) {
+				t.Errorf("reverseList() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_reverseListV3(t *testing.T) {
+	type args struct {
+		head []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			name: "lc case 1",
+			args: args{
+				head: []int{1, 2, 3, 4, 5},
+			},
+			want: []int{5, 4, 3, 2, 1},
+		},
+		{
+			name: "lc case 2",
+			args: args{
+				head: []int{1, 2},
+			},
+			want: []int{2, 1},
+		},
+		{
+			name: "lc case 3",
+			args: args{
+				head: []int{},
+			},
+			want: []int{},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			list := ConstructListNodeFromSlice(tt.args.head)
+			got := reverseListV3(list)
+			want := ConstructListNodeFromSlice(tt.want)
+			if !reflect.DeepEqual(got, want) {
+				t.Errorf("reverseList() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_reverseListFirstN(t *testing.T) {
+	type args struct {
+		head []int
+		n    int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			name: "case 1",
+			args: args{
+				head: []int{1, 2, 3, 4, 5},
+				n:    2,
+			},
+			want: []int{2, 1, 3, 4, 5},
+		},
+		{
+			name: "case 2",
+			args: args{
+				head: []int{1, 2, 3, 4, 5},
+				n:    5,
+			},
+			want: []int{5, 4, 3, 2, 1},
+		},
+		{
+			name: "case 3",
+			args: args{
+				head: []int{1, 2},
+				n:    0,
+			},
+			want: []int{1, 2},
+		},
+		{
+			name: "case 4",
+			args: args{
+				head: []int{1, 2},
+				n:    2,
+			},
+			want: []int{2, 1},
+		},
+		{
+			name: "case 5",
+			args: args{
+				head: []int{},
+				n:    2,
+			},
+			want: []int{},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			list := ConstructListNodeFromSlice(tt.args.head)
+			want := ConstructListNodeFromSlice(tt.want)
+			if got := reverseListFirstN(list, tt.args.n); !reflect.DeepEqual(got, want) {
+				t.Errorf("reverseListFirstN() = %v, want %v", tt.args.head, tt.want)
+			}
+		})
+	}
+}
+
+func Test_reverseListFirstNHelper(t *testing.T) {
+	type args struct {
+		head *ListNode
+		n    int
+	}
+	tests := []struct {
+		name  string
+		args  args
+		want  *ListNode
+		want1 *ListNode
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, got1 := reverseListFirstNHelper(tt.args.head, tt.args.n)
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("reverseListFirstNHelper() got = %v, want %v", got, tt.want)
+			}
+			if !reflect.DeepEqual(got1, tt.want1) {
+				t.Errorf("reverseListFirstNHelper() got1 = %v, want %v", got1, tt.want1)
+			}
+		})
+	}
+}
