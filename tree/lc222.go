ff4d773ae1f541b7b4d60934af2fbb2e89474131
brofu <brofu.talk@gmail.com>
2023-12-13 18:31:37 +0800

    Tree-lc222. Count Complete Tree Nodes. Add Perfect Binary Tree, Full Binary Tree and Complete Binary Tree

--- tree/lc222.go
+++ tree/lc222.go
@@ -1,5 +1,8 @@
 package tree
 
+import "math"
+
+// O(n)
 func countNodes(root *TreeNode) int {
 
 	if root == nil {
@@ -7,4 +10,42 @@ func countNodes(root *TreeNode) int {
 	}
 
 	return 1 + countNodes(root.Left) + countNodes(root.Right)
+}
+
+// if a perfect binary tree
+// O(logN)
+func countNodesPerfectBinaryTree(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	h := 0
+	for ; root != nil; root = root.Left {
+		h += 1
+	}
+	return int(math.Pow(2, float64(h))) - 1
+}
+
+// O(lgN*lgN)
+func countNodesCompleteBinaryTree(root *TreeNode) int {
+
+	if root == nil {
+		return 0
+	}
+
+	left, right := 0, 0
+
+	for lt := root; lt != nil; lt = lt.Left {
+		left += 1
+	}
+
+	for rt := root; rt != nil; rt = rt.Right {
+		right += 1
+	}
+
+	if left == right {
+		return int(math.Pow(2, float64(left))) - 1
+	}
+
+	return 1 + countNodesCompleteBinaryTree(root.Left) + countNodesCompleteBinaryTree(root.Right)
 }
--- tree/lc222_test.go
+++ tree/lc222_test.go
@@ -1,6 +1,8 @@
 package tree
 
-import "testing"
+import (
+	"testing"
+)
 
 func Test_countNodes(t *testing.T) {
 	type args struct {
@@ -38,6 +40,88 @@ func Test_countNodes(t *testing.T) {
 			tree := GenerateBinaryTreeFromSlice(tt.args.root, 0)
 			if got := countNodes(tree); got != tt.want {
 				t.Errorf("countNodes() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_countNodesPerfectBinaryTree(t *testing.T) {
+	type args struct {
+		root []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "lc case 1",
+			args: args{
+				root: []int{1, 2, 3, 4, 5, 6, 7},
+			},
+			want: 7,
+		},
+		{
+			name: "lc case 2",
+			args: args{
+				root: []int{},
+			},
+			want: 0,
+		},
+		{
+			name: "lc case 3",
+			args: args{
+				root: []int{1},
+			},
+			want: 1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tree := GenerateBinaryTreeFromSlice(tt.args.root, 0)
+			if got := countNodesPerfectBinaryTree(tree); got != tt.want {
+				t.Errorf("countNodesPerfectBinaryTree() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_countNodesCompleteBinaryTree(t *testing.T) {
+	type args struct {
+		root []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "lc case 1",
+			args: args{
+				root: []int{1, 2, 3, 4, 5, 6},
+			},
+			want: 6,
+		},
+		{
+			name: "lc case 2",
+			args: args{
+				root: []int{},
+			},
+			want: 0,
+		},
+		{
+			name: "lc case 3",
+			args: args{
+				root: []int{1},
+			},
+			want: 1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tree := GenerateBinaryTreeFromSlice(tt.args.root, 0)
+			if got := countNodesCompleteBinaryTree(tree); got != tt.want {
+				t.Errorf("countNodesCompleteBinaryTree() = %v, want %v", got, tt.want)
 			}
 		})
 	}
