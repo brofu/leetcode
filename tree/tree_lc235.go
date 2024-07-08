d941024829fc51cd27597f44b54e96b0b597332a
brofu <brofu.talk@gmail.com>
2024-07-08 12:53:54 +0800

    tree. 235. Lowest Common Ancestor of a Binary Search Tree. PV1

--- tree/tree_lc235.go
+++ tree/tree_lc235.go
@@ -27,3 +27,32 @@ func lowestCommonAncestorBST(root, p, q *TreeNode) *TreeNode {
 
 	return traverse(root, p, q)
 }
+
+func lowestCommonAncestorBSTPV1(root, p, q *TreeNode) *TreeNode {
+
+	var traverse func(*TreeNode, *TreeNode, *TreeNode) *TreeNode
+
+	traverse = func(root, p, q *TreeNode) *TreeNode {
+		if root == nil {
+			return root
+		}
+
+		small, big := p, q
+		if p.Val > q.Val {
+			big = p
+			small = q
+		}
+
+		if root.Val >= small.Val && root.Val <= big.Val {
+			return root
+		}
+
+		if root.Val < small.Val {
+			return traverse(root.Right, p, q)
+		} else {
+			return traverse(root.Left, p, q)
+		}
+	}
+
+	return traverse(root, p, q)
+}
