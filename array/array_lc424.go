80361176565f4d59beff7e157a29fd7b09319772
brofu <brofu.talk@gmail.com>
2024-07-23 16:04:34 +0800

    array. 424. Longest Repeating Character Replacement. Sliding Window

---
+++ array/array_lc424.go
@@ -0,0 +1,96 @@
+package array
+
+import "github.com/brofu/leetcode/common"
+
+func characterReplacement(s string, k int) int {
+
+	result := 0
+	unique := make(map[byte]int)
+
+	type subMax struct {
+		count int
+		rs    map[byte]struct{}
+	}
+
+	subMaxR := subMax{0, make(map[byte]struct{})}
+	subSum := 0
+
+	for left, right := 0, 0; right < len(s); right++ {
+
+		// extend the window
+		r := s[right]
+		subSum++
+		unique[r]++
+
+		if subMaxR.count < unique[r] {
+			subMaxR.count = unique[r]
+			subMaxR.rs = make(map[byte]struct{}) // a new record
+			subMaxR.rs[r] = struct{}{}
+		}
+
+		if subMaxR.count == unique[r] {
+			subMaxR.rs[r] = struct{}{}
+		}
+
+		// shrunk window
+		for subSum-subMaxR.count > k {
+			o := s[left]
+			subSum--
+			if unique[o] == subMaxR.count { // o also has max count
+				delete(subMaxR.rs, o)
+				if len(subMaxR.rs) == 0 { // search the new max value
+					subMaxR.count = 0
+					for k, v := range unique {
+						if subMaxR.count < v {
+							subMaxR.count = v
+							subMaxR.rs[k] = struct{}{}
+						}
+						if subMaxR.count == v {
+							subMaxR.rs[k] = struct{}{}
+						}
+					}
+				}
+			}
+			unique[o]--
+			left++
+		}
+
+		if result < right-left+1 {
+			result = right - left + 1
+		}
+	}
+
+	return result
+}
+
+// The brief version
+/**
+KP
+	*	Utilize the `index` of the character in s. For example,
+		*	the sum of the characters in the window,
+		*	the count of other character except the one which as largest frequency.
+*/
+func characterReplacementV2(s string, k int) int {
+	result := 0
+
+	maxFrequency := 0
+	frequency := make(map[byte]int) // KP. array has better performance.
+	for left, right := 0, 0; right < len(s); right++ {
+
+		// extend the window
+		r := s[right]
+		frequency[r]++
+		maxFrequency = common.MaxInt(maxFrequency, frequency[r])
+
+		// shrunk the window
+		if right-left+1-maxFrequency > k {
+			o := s[left]
+			frequency[o]--
+			left++
+		}
+
+		result = common.MaxInt(result, right-left+1)
+	}
+
+	return result
+}
---
+++ array/array_lc424_test.go
@@ -0,0 +1,77 @@
+package array
+
+import (
+	"testing"
+)
+
+func Test_characterReplacement(t *testing.T) {
+	type args struct {
+		s string
+		k int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "lc case 1",
+			args: args{
+				s: "ABAB",
+				k: 2,
+			},
+			want: 4,
+		},
+		{
+			name: "lc case 2",
+			args: args{
+				s: "AABABBA",
+				k: 1,
+			},
+			want: 4,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := characterReplacement(tt.args.s, tt.args.k); got != tt.want {
+				t.Errorf("characterReplacement() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_characterReplacementV2(t *testing.T) {
+	type args struct {
+		s string
+		k int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "lc case 1",
+			args: args{
+				s: "ABAB",
+				k: 2,
+			},
+			want: 4,
+		},
+		{
+			name: "lc case 2",
+			args: args{
+				s: "AABABBA",
+				k: 1,
+			},
+			want: 4,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := characterReplacementV2(tt.args.s, tt.args.k); got != tt.want {
+				t.Errorf("characterReplacementV2() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
