2ad18593745275a9eb860bedb41bd380cc35a93c
brofu <brofu.talk@gmail.com>
2024-07-24 14:40:30 +0800

    graph. 797. All Paths From Source to Target. PV1

--- graph/graph_lc797.go
+++ graph/graph_lc797.go
@@ -26,3 +26,31 @@ func allPathsSourceTarget(graph [][]int) [][]int {
 
 	return result
 }
+
+func allPathsSourceTargetPV1(graph [][]int) [][]int {
+
+	result := [][]int{}
+	path := []int{}
+	var dfs func([]int, int)
+	dfs = func(path []int, node int) {
+
+		// choose
+		path = append(path, node)
+
+		if node == len(graph)-1 {
+			result = append(result, append([]int{}, path...))
+			return
+		}
+		// next layer
+		for _, i := range graph[node] {
+			dfs(path, i)
+		}
+
+		// cancel choose
+		path = path[:len(path)-1]
+	}
+
+	dfs(path, 0)
+
+	return result
+}
