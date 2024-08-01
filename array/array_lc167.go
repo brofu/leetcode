5105fb83491dc745d93c33bc3a539183c16c6e0d
brofu <brofu.talk@gmail.com>
2024-08-01 16:38:16 +0800

    array. 167. Two Sum II - Input Array Is Sorted. Double Pointers

--- array/array_lc167.go
+++ array/array_lc167.go
@@ -1,1 +1,20 @@
 package array
+
+func twoSum167(numbers []int, target int) []int {
+
+	result := []int{}
+	for left, right := 0, len(numbers)-1; left <= right; {
+		sum := numbers[left] + numbers[right]
+		if sum == target {
+			result = []int{left + 1, right + 1}
+			break
+		}
+		if sum < target {
+			left++
+		} else {
+			right--
+		}
+	}
+
+	return result
+}
