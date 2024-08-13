ad66240b539009fa69fa54cc65e6d98f3f9f51f3
brofu <brofu.talk@gmail.com>
2024-08-13 16:36:10 +0800

    dp. 518. Coin Change II. Knapsack Problem. 完全背包问题.

---
+++ dp/dp_lc518.go
@@ -0,0 +1,32 @@
+package dp
+
+/**
+KP.
+	1.	Difference from 0-1 Knapsack Problem?
+		a.	No number limitation of each coin
+		b.	The status transfer is Different from the 0-1 problem.
+*/
+
+func change(amount int, coins []int) int {
+
+	dp := make([][]int, len(coins)+1)
+	for i := range dp {
+		dp[i] = make([]int, amount+1)
+		dp[i][0] = 1 // base case 1. if amount = 0, 1 solution is to do nothing.
+	}
+	for j := 0; j <= amount; j++ {
+		dp[0][j] = 0 // base case 1. if no coin at all, no solution to get the amount
+	}
+
+	for i := 1; i <= len(coins); i++ {
+		for j := 1; j <= amount; j++ {
+			if j-coins[i-1] < 0 { // bag space NOT enough
+				dp[i][j] = dp[i-1][j]
+			} else { // pick up coins[i-1] AND NOT pick up coins[i-1]. All possible solutions
+				dp[i][j] = dp[i-1][j] + dp[i][j-coins[i-1]]
+			}
+		}
+	}
+
+	return dp[len(coins)][amount]
+}
