22d0056e91493ada572037e86c91fc5d3796b895
brofu <brofu.talk@gmail.com>
2024-08-13 12:09:59 +0800

    dp. 712. Minimum ASCII Delete Sum for Two Strings. DP function. DP 2-D table. DP 1-D table.

--- dp/dp_lc712.go
+++ dp/dp_lc712.go
@@ -4,6 +4,11 @@ import (
 	"github.com/brofu/leetcode/common"
 )
 
+/**
+KP.
+	1.	Similar as 583 and 1143. But cannot get resolved by reusing LCS
+	2.	Return the min sum from DP function.
+*/
 func minimumDeleteSum(s1 string, s2 string) int {
 
 	memo := make([][]int, len(s1))
@@ -51,4 +56,77 @@ func minimumDeleteSum(s1 string, s2 string) int {
 	}
 
 	return dp(0, 0)
+}
+
+/**
+KP.
+	1.	Similar as 583 and 1143. But cannot get resolved by reusing LCS
+	2.	Record the min sum in DP table
+*/
+
+func minimumDeleteSumDPTable(s1 string, s2 string) int {
+	m, n := len(s1), len(s2)
+
+	dp := make([][]int, m+1)
+	for i := range dp {
+		dp[i] = make([]int, n+1)
+	}
+
+	// base case 1
+	for i := 1; i <= m; i++ {
+		dp[i][0] = int(s1[i-1]) + dp[i-1][0]
+	}
+
+	// base case 2
+	for j := 1; j <= n; j++ {
+		dp[0][j] = int(s2[j-1]) + dp[0][j-1]
+	}
+
+	for i := 1; i <= m; i++ {
+		for j := 1; j <= n; j++ {
+			if s1[i-1] == s2[j-1] {
+				dp[i][j] = dp[i-1][j-1]
+			} else {
+				dp[i][j] = common.MinInt(
+					int(s1[i-1])+dp[i-1][j], // delete s1[i]
+					int(s2[j-1])+dp[i][j-1], // delete s2[j]
+				)
+			}
+		}
+	}
+
+	return dp[m][n]
+}
+
+/**
+DP table version, with space compress
+*/
+func minimumDeleteSumDPTableWithSpaceCompress(s1 string, s2 string) int {
+
+	dp := make([]int, len(s2)+1)
+
+	// base case
+	for j := 1; j <= len(s2); j++ {
+		dp[j] = dp[j-1] + int(s2[j-1])
+	}
+
+	for i := 1; i <= len(s1); i++ {
+		ph := dp[0]
+		dp[0] += int(s1[i-1])
+
+		for j := 1; j <= len(s2); j++ {
+			tmp := dp[j]
+			if s1[i-1] == s2[j-1] {
+				dp[j] = ph
+			} else {
+				dp[j] = common.MinInt(
+					int(s1[i-1])+dp[j],   // delete s1[i]. dp[j] actually dp[i-1][j] before updated
+					int(s2[j-1])+dp[j-1], // delete s2[j], dp[j-1] actually dp[i][j-1]
+				)
+			}
+			ph = tmp // ph is actually dp[i-1][j-1], for the next new j
+		}
+
+	}
+	return dp[len(s2)]
 }
--- dp/dp_lc712_test.go
+++ dp/dp_lc712_test.go
@@ -1,6 +1,8 @@
 package dp
 
-import "testing"
+import (
+	"testing"
+)
 
 func Test_minimumDeleteSum(t *testing.T) {
 	type args struct {
@@ -33,6 +35,86 @@ func Test_minimumDeleteSum(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := minimumDeleteSum(tt.args.s1, tt.args.s2); got != tt.want {
 				t.Errorf("minimumDeleteSum() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_minimumDeleteSumDPTable(t *testing.T) {
+	type args struct {
+		s1 string
+		s2 string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "lc case 1",
+			args: args{
+				s1: "sea",
+				s2: "eat",
+			},
+			want: 231,
+		},
+		{
+			name: "lc case 2",
+			args: args{
+				s1: "delete",
+				s2: "leet",
+			},
+			want: 403,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := minimumDeleteSumDPTable(tt.args.s1, tt.args.s2); got != tt.want {
+				t.Errorf("minimumDeleteSumDPTable() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_minimumDeleteSumDPTableWithSpaceCompress(t *testing.T) {
+	type args struct {
+		s1 string
+		s2 string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "lc case 1",
+			args: args{
+				s1: "sea",
+				s2: "eat",
+			},
+			want: 231,
+		},
+		{
+			name: "lc case 2",
+			args: args{
+				s1: "delete",
+				s2: "leet",
+			},
+			want: 403,
+		},
+		{
+			name: "lc case 3",
+			args: args{
+				s1: "a",
+				s2: "at",
+			},
+			want: 116,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := minimumDeleteSumDPTableWithSpaceCompress(tt.args.s1, tt.args.s2); got != tt.want {
+				t.Errorf("minimumDeleteSumDPTableWithSpaceCompress() = %v, want %v", got, tt.want)
 			}
 		})
 	}
