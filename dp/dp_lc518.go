fc31f22911b4de25440d20a8c11a275cb328d3d1
brofu <brofu.talk@gmail.com>
2024-08-14 11:08:02 +0800

    dp. 518. Coin Change II. 2D DP table. 1D DP table

--- dp/dp_lc518.go
+++ dp/dp_lc518.go
@@ -30,3 +30,22 @@ func change(amount int, coins []int) int {
 
 	return dp[len(coins)][amount]
 }
+
+/**
+Space Compression Version.
+KP.
+	1.	When to update dp[0]?
+*/
+func changeWithSpaceCompression(amount int, coins []int) int {
+	dp := make([]int, amount+1)
+	dp[0] = 0 // KP. dp[i][0] = 0, if i = 0
+	for i := 1; i <= len(coins); i++ {
+		dp[0] = 1 // KP. dp[i][0] = 1, if i > 0.
+		for j := 1; j <= amount; j++ {
+			if j-coins[i-1] >= 0 { // KP. if j-coins[i-1] < 0, dp[j] = dp[j]
+				dp[j] = dp[j] + dp[j-coins[i-1]]
+			}
+		}
+	}
+	return dp[amount]
+}
--- dp/dp_lc518_test.go
+++ dp/dp_lc518_test.go
@@ -1,6 +1,8 @@
 package dp
 
-import "testing"
+import (
+	"testing"
+)
 
 func Test_change(t *testing.T) {
 	type args struct {
@@ -41,6 +43,58 @@ func Test_change(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := change(tt.args.amount, tt.args.coins); got != tt.want {
 				t.Errorf("change() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_changeWithSpaceCompression(t *testing.T) {
+	type args struct {
+		amount int
+		coins  []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "lc case 1",
+			args: args{
+				amount: 5,
+				coins:  []int{1, 2, 5},
+			},
+			want: 4,
+		},
+		{
+			name: "lc case 2",
+			args: args{
+				amount: 3,
+				coins:  []int{2},
+			},
+			want: 0,
+		},
+		{
+			name: "lc case 3",
+			args: args{
+				amount: 10,
+				coins:  []int{10},
+			},
+			want: 1,
+		},
+		{
+			name: "lc case 4",
+			args: args{
+				amount: 5,
+				coins:  []int{2, 5},
+			},
+			want: 1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := changeWithSpaceCompression(tt.args.amount, tt.args.coins); got != tt.want {
+				t.Errorf("changeWithSpaceCompression() = %v, want %v", got, tt.want)
 			}
 		})
 	}
