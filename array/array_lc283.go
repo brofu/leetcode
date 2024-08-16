bab4c47f177202fbd5acf524d19b10456e3da64f
brofu <brofu.talk@gmail.com>
2024-08-16 13:01:29 +0800

    array. 283. Move Zeroes. Double Pointers. PV1

--- array/array_lc283.go
+++ array/array_lc283.go
@@ -15,3 +15,12 @@ func moveZeroes(nums []int) {
 		nums[i] = 0
 	}
 }
+
+func moveZeroesPV1(nums []int) {
+	for i, j := 0, 0; i < len(nums); i++ { // similar as the logic in pivot func in quick sort
+		if nums[i] != 0 {
+			nums[i], nums[j] = nums[j], nums[i]
+			j++
+		}
+	}
+}
--- array/array_lc283_test.go
+++ array/array_lc283_test.go
@@ -39,3 +39,38 @@ func Test_moveZeroes(t *testing.T) {
 		})
 	}
 }
+
+func Test_moveZeroesPV1(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			name: "lc case 1",
+			args: args{
+				nums: []int{0, 1, 0, 3, 12},
+			},
+			want: []int{1, 3, 12, 0, 0},
+		},
+		{
+			name: "lc case 2",
+			args: args{
+				nums: []int{0},
+			},
+			want: []int{0},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			moveZeroesPV1(tt.args.nums)
+			for i := 0; i < len(tt.args.nums); i++ {
+				assert.Equal(t, tt.want[i], tt.args.nums[i])
+			}
+
+		})
+	}
+}
