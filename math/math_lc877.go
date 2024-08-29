b8a9aa0d6c755adeb15a6fe3f6197bd999a48e77
brofu <brofu.talk@gmail.com>
2024-08-29 13:15:30 +0800

    math. 877. Stone Game

--- math/math_lc877.go
+++ math/math_lc877.go
@@ -1,5 +1,14 @@
 package math
 
+/**
+
+KP.
+	How Alice can always win if she can take first?
+	1.	Check the stone sum of `ODD` and `EVEN` number index of the piles. That's sum(1st, 3rd, 5th, 7th...) and sum(2nd, 4th, 6th, 8th...)
+	2.	If ODD sum > EVEN sum, take from the 1st pile. (Then, Bob can only take a EVEN index pile, the 2nd, or Nth)
+	3.  If EVEN sum > ODD sum, take from the last pile (an EVEN pile). (Then, Bob can only take an ODD index pile, the 1st or N-1th)
+*/
+
 func stoneGame(piles []int) bool {
 	return true
 }
