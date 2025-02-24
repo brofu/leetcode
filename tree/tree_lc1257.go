302be6fc3d9fc4d97ead9eb5016b5bf35cf1554e
brofu <brofu.talk@gmail.com>
2025-02-24 11:54:46 +0800

    tree. 1257. Smallest Common Region. Actually a link list problem

--- tree/tree_lc1257.go
+++ tree/tree_lc1257.go
@@ -29,3 +29,30 @@ func findSmallestRegion(regions [][]string, region1 string, region2 string) stri
 
 	return region1p
 }
+
+func findSmallestRegionPV2(regions [][]string, region1 string, region2 string) string {
+
+	parentMap := make(map[string]string)
+
+	for _, list := range regions {
+		for idx := 1; idx < len(list); idx++ {
+			parentMap[list[idx]] = list[0]
+		}
+	}
+	a, b := region1, region2
+
+	for a != b {
+		if a == "" {
+			a = region2
+		} else {
+			a, _ = parentMap[a]
+		}
+
+		if b == "" {
+			b = region1
+		} else {
+			b, _ = parentMap[b]
+		}
+	}
+	return a
+}
