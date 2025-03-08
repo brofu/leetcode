47e617fe2cd466ac30ebd222b0096ddedaf9e638
brofu <brofu.talk@gmail.com>
2025-03-08 11:11:50 +0800

    tree 106. Construct Binary Tree from Inorder and Postorder Traversal. Sub Task; HashMap

--- tree/tree_lc106.go
+++ tree/tree_lc106.go
@@ -28,3 +28,33 @@ func buildTreeSubTaskPV1(inorder []int, postorder []int) *TreeNode {
 	}
 	return traverse(inorder, postorder)
 }
+
+/*
+Sub Task
+Use map to reduce the time complexity
+*/
+func buildTreeSubTaskPV3(inorder []int, postorder []int) *TreeNode {
+
+	var traverse func([]int, []int) *TreeNode
+
+	traverse = func(inorder, postorder []int) *TreeNode {
+		if len(postorder) == 0 {
+			return nil
+		}
+
+		root := &TreeNode{
+			Val: postorder[len(postorder)-1],
+		}
+		index := -1
+		for idx, num := range inorder {
+			if num == root.Val {
+				index = idx
+				break
+			}
+		}
+		root.Left = traverse(inorder[:index], postorder[:index])
+		root.Right = traverse(inorder[index+1:], postorder[index:len(postorder)-1])
+		return root
+	}
+	return traverse(inorder, postorder)
+}
