4b4709bc6fb1c4dde28941ecc545962aee0041e0
brofu <brofu.talk@gmail.com>
2025-03-13 21:25:03 +0800

    tree. 102. Binary Tree Level Order Traversal. BFS

--- tree/tree_lc102.go
+++ tree/tree_lc102.go
@@ -1,6 +1,34 @@
 package tree
 
+/*
+BFS
+*/
 func levelOrder(root *TreeNode) [][]int {
 
-	return nil
+	result := [][]int{}
+	if root == nil {
+		return result
+	}
+
+	q := []*TreeNode{root}
+
+	for len(q) > 0 {
+
+		size := len(q)
+		temp := make([]int, size)
+		for idx := 0; idx < size; idx++ {
+			node := q[idx]
+			temp[idx] = node.Val
+			if node.Left != nil {
+				q = append(q, node.Left)
+			}
+			if node.Right != nil {
+				q = append(q, node.Right)
+			}
+		}
+		result = append(result, temp)
+		q = q[size:]
+	}
+
+	return result
 }
