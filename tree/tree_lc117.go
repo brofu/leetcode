eab9764efcadd64c8da82b91de4cd755bd6d1b6e
brofu <brofu.talk@gmail.com>
2025-03-15 12:02:43 +0800

    tree. 117. Populating Next Right Pointers in Each Node II. Pointer appraoch

--- tree/tree_lc117.go
+++ tree/tree_lc117.go
@@ -37,3 +37,56 @@ func connectPV1(root *Node) *Node {
 
 	return root
 }
+
+/*
+Follow-up: You may only use constant extra space.
+
+Question:
+How to resolve the problem with constant extra space?
+
+Pointers
+
+	Key Points
+	1. 3 important pointers
+		1. The `leftmost` node at each layer
+		2. The `current` node within each layer
+		3. The `pre` node of `current` node
+
+	2. Connect the `children` at the `parent` layer and then, loop the `children` layer
+*/
+func connectWtihConstantSpace(root *Node) *Node {
+
+	if root == nil {
+		return root
+	}
+
+	leftmost := root
+
+	for leftmost != nil {
+		cur := leftmost
+		leftmost = nil
+		var pre *Node
+		for cur != nil {
+			pre, leftmost = processChildren(cur.Left, pre, leftmost)
+			pre, leftmost = processChildren(cur.Right, pre, leftmost)
+			cur = cur.Next
+		}
+	}
+
+	return root
+}
+
+func processChildren(cur, pre, leftmost *Node) (*Node, *Node) {
+
+	if cur == nil {
+		return pre, leftmost
+	}
+
+	if pre != nil {
+		pre.Next = cur
+	} else {
+		leftmost = cur
+	}
+	pre = cur
+	return pre, leftmost
+}
