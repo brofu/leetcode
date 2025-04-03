84dd6daa0454692a3d4667713869f4b9fe57c4c5
brofu <brofu.talk@gmail.com>
2025-04-03 18:52:24 +0800

    207. Course Schedule. DFS

--- graph/graph_lc207.go
+++ graph/graph_lc207.go
@@ -43,3 +43,59 @@ func canFinish(numCourses int, prerequisites [][]int) bool {
 	}
 	return !ring
 }
+
+/*
+Key Point
+
+The order of checking `visited` and `onPath` is important
+*/
+
+func canFinishPV1(numCourses int, prerequisites [][]int) bool {
+
+	// construct the graph
+	graph := make([][]int, numCourses)
+	for _, record := range prerequisites {
+		from, to := record[1], record[0]
+		graph[from] = append(graph[from], to)
+	}
+
+	var traverse func(int)
+
+	hasCycle := false
+	onPath := make([]bool, len(graph))
+	visited := make([]bool, len(graph))
+
+	traverse = func(val int) {
+
+		// base case
+		if hasCycle {
+			return
+		}
+
+		// get ring
+		if onPath[val] {
+			hasCycle = true
+			return
+		}
+
+		if visited[val] {
+			return
+		}
+
+		// pre-order location
+		onPath[val] = true
+		visited[val] = true
+		for _, number := range graph[val] {
+			traverse(number)
+		}
+
+		// post-order location
+		onPath[val] = false
+	}
+
+	for index := range graph {
+		traverse(index)
+	}
+
+	return !hasCycle
+}
--- graph/graph_lc207_test.go
+++ graph/graph_lc207_test.go
@@ -1,6 +1,8 @@
 package graph
 
-import "testing"
+import (
+	"testing"
+)
 
 func Test_canFinish(t *testing.T) {
 	type args struct {
@@ -33,6 +35,42 @@ func Test_canFinish(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := canFinish(tt.args.numCourses, tt.args.prerequisites); got != tt.want {
 				t.Errorf("canFinish() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_canFinishPV1(t *testing.T) {
+	type args struct {
+		numCourses    int
+		prerequisites [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "lc case 1",
+			args: args{
+				numCourses:    2,
+				prerequisites: [][]int{{1, 0}},
+			},
+			want: true,
+		},
+		{
+			name: "lc case 2",
+			args: args{
+				numCourses:    2,
+				prerequisites: [][]int{{1, 0}, {0, 1}},
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := canFinishPV1(tt.args.numCourses, tt.args.prerequisites); got != tt.want {
+				t.Errorf("canFinishPV1() = %v, want %v", got, tt.want)
 			}
 		})
 	}
