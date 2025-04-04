2418ca7e2ddfac7bb528b758e8b19a99a8c96ae4
brofu <brofu.talk@gmail.com>
2025-04-04 12:09:44 +0800

    graph. 886. Possible Bipartition. DFS and BFS

--- graph/graph_lc886.go
+++ graph/graph_lc886.go
@@ -1,9 +1,10 @@
 package graph
 
-/**
+/*
+*
 KP.
-	1.	How to transfer the problem to Bipartite problem?
-	如果把每个人看做图中的节点，相互讨厌的关系看做图中的边，那么 dislikes 数组就可以构成一幅图；又因为题目说互相讨厌的人不能放在同一组里，相当于图中的所有相邻节点都要放进两个不同的组； 那就回到了「双色问题」，如果能够用两种颜色着色所有节点，且相邻节点颜色都不同，那么按照颜色把这些节点分成两组就行了。 所以解法是，dislikes 构造成一幅图，然后执行二分图的判定算法即可
+ 1. How to transfer the problem to Bipartite problem?
+    如果把每个人看做图中的节点，相互讨厌的关系看做图中的边，那么 dislikes 数组就可以构成一幅图；又因为题目说互相讨厌的人不能放在同一组里，相当于图中的所有相邻节点都要放进两个不同的组； 那就回到了「双色问题」，如果能够用两种颜色着色所有节点，且相邻节点颜色都不同，那么按照颜色把这些节点分成两组就行了。 所以解法是，dislikes 构造成一幅图，然后执行二分图的判定算法即可
 */
 func possibleBipartition(n int, dislikes [][]int) bool {
 
@@ -52,4 +53,111 @@ func possibleBipartition(n int, dislikes [][]int) bool {
 	}
 
 	return result
+}
+
+func possibleBipartitionDFSPV1(n int, dislikes [][]int) bool {
+
+	// construct un-directed graph
+	graph := make([][]int, n+1)
+	for _, record := range dislikes {
+		from, to := record[0], record[1]
+		graph[from] = append(graph[from], to)
+		graph[to] = append(graph[to], from)
+	}
+
+	result := true
+
+	// traverse
+	var traverse func(v int)
+
+	visited := make([]bool, n+1)
+	color := make([]bool, n+1)
+
+	traverse = func(v int) {
+
+		// base case
+		if !result {
+			return
+		}
+
+		if visited[v] {
+			return
+		}
+
+		// pre-order location
+		visited[v] = true
+
+		for _, next := range graph[v] {
+			if visited[next] {
+				if color[next] == color[v] {
+					result = false
+					return
+				}
+			} else {
+				color[next] = !color[v]
+				traverse(next)
+			}
+		}
+	}
+
+	for idx := range graph {
+		traverse(idx)
+	}
+
+	return result
+}
+
+func possibleBipartitionBFSPV1(n int, dislikes [][]int) bool {
+
+	// construct un-directed graph
+	graph := make([][]int, n+1)
+	for _, record := range dislikes {
+		from, to := record[0], record[1]
+		graph[from] = append(graph[from], to)
+		graph[to] = append(graph[to], from)
+	}
+
+	result := true
+	visited := make([]bool, n+1)
+	color := make([]bool, n+1)
+
+	traverse := func(v int) {
+
+		if !result {
+			return
+		}
+
+		q := []int{v}
+		visited[v] = true
+
+		for len(q) > 0 {
+			size := len(q)
+			for idx := 0; idx < size; idx++ {
+				v := q[idx]
+
+				for _, next := range graph[v] {
+					if visited[next] {
+						if color[next] == color[v] {
+							result = false
+							return
+						}
+					} else {
+						color[next] = !color[v]
+						visited[next] = true
+						q = append(q, next)
+					}
+				}
+			}
+			q = q[size:]
+		}
+	}
+
+	for idx := range graph {
+		if !visited[idx] {
+			traverse(idx)
+		}
+	}
+
+	return result
+
 }
--- graph/graph_lc886_test.go
+++ graph/graph_lc886_test.go
@@ -1,6 +1,8 @@
 package graph
 
-import "testing"
+import (
+	"testing"
+)
 
 func Test_possibleBipartition(t *testing.T) {
 	type args struct {
@@ -37,6 +39,86 @@ func Test_possibleBipartition(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := possibleBipartition(tt.args.n, tt.args.dislikes); got != tt.want {
 				t.Errorf("possibleBipartition() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_possibleBipartitionDFSPV1(t *testing.T) {
+	type args struct {
+		n        int
+		dislikes [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "lc case 1",
+			args: args{
+				n: 4,
+				dislikes: [][]int{
+					{1, 2}, {1, 3}, {2, 4},
+				},
+			},
+			want: true,
+		},
+		{
+			name: "lc case 2",
+			args: args{
+				n: 3,
+				dislikes: [][]int{
+					{1, 2}, {1, 3}, {2, 3},
+				},
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := possibleBipartitionDFSPV1(tt.args.n, tt.args.dislikes); got != tt.want {
+				t.Errorf("possibleBipartitionDFSPV1() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_possibleBipartitionBFSPV1(t *testing.T) {
+	type args struct {
+		n        int
+		dislikes [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "lc case 1",
+			args: args{
+				n: 4,
+				dislikes: [][]int{
+					{1, 2}, {1, 3}, {2, 4},
+				},
+			},
+			want: true,
+		},
+		{
+			name: "lc case 2",
+			args: args{
+				n: 3,
+				dislikes: [][]int{
+					{1, 2}, {1, 3}, {2, 3},
+				},
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := possibleBipartitionBFSPV1(tt.args.n, tt.args.dislikes); got != tt.want {
+				t.Errorf("possibleBipartitionDFSPV1() = %v, want %v", got, tt.want)
 			}
 		})
 	}
