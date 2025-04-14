8f3a195405916f1c8fa76fb6e0e025c9cdff80a4
brofu <brofu.talk@gmail.com>
2025-04-14 20:11:12 +0800

    graph. Minimum spanning tree. 1584. Min Cost to Connect All Points. Optimization

--- graph/mst/mst_lc1584.go
+++ graph/mst/mst_lc1584.go
@@ -2,9 +2,10 @@ package mst
 
 import (
 	"container/heap"
-	"fmt"
+	"math"
 	"sort"
 
+	"github.com/brofu/leetcode/common"
 	heaplib "github.com/brofu/leetcode/common/heap"
 	uflib "github.com/brofu/leetcode/graph/union_find"
 )
@@ -56,6 +57,10 @@ func Abs(a, b int) int {
 /*
 Prime
 
+Time Complexity: O(N^2logN)
+ 1. Construct a full graph, there is N^2 sides
+ 2. Priority Queue operations O(N^2logN)
+
 Key Point:
 1. Use the index in the points array as the node
 2. MUST construct the FULL graph (ALL the side between any two nodes. Otherwise, there would be sides missing. Refer to the case in case 1
@@ -72,8 +77,6 @@ func minCostConnectPointsPrim(points [][]int) int {
 		}
 	}
 
-	fmt.Println("flag", points)
-	fmt.Println("flag", graph)
 	pq := heaplib.NewInterfacePriorityQueue(func(data []interface{}, i, j int) bool {
 		return data[i].([]int)[2] < data[j].([]int)[2]
 	})
@@ -105,14 +108,68 @@ func minCostConnectPointsPrim(points [][]int) int {
 		}
 		inMST[to] = true
 		sum += weight
-		fmt.Println("flag2", sum, current)
 		cut(to)
 	}
 
 	return sum
 }
 
-func minCostConnectPointsPrimV2(points [][]int) int {
+/*
+minCostConnectPointsPrimOptimization optimize the Prim version
 
-	return 0
+Time Complexity: O(n^2)
+
+How does the optimization work?
+
+ 1. No need to construct the FULL graph for this scenario, since in this scenarios, we are sure there all the nodes should be in the graph
+ 2. So, we are use another description of `Prim` algorithm
+    a. `dist[i]` means, the shorted distance between the node `i` and the `mst` (all the nodes in the `mst`)
+    b. all the nodes should be in the `mst` tree, so, we have `for edge < n-1` loop
+*/
+
+func minCostConnectPointsPrimOptimization(points [][]int) int {
+
+	n := len(points)
+
+	visisted := make([]bool, n)
+	dist := make([]int, n)
+	for idx := range dist {
+		dist[idx] = math.MaxInt32
+	}
+
+	edge, node, sum := 0, 0, 0
+
+	for edge < n-1 {
+
+		visisted[node] = true // put node into mst
+		nextNode := -1
+
+		for idx := 0; idx < n; idx++ {
+
+			if visisted[idx] {
+				continue
+			}
+
+			// calculate the distance between i and node
+			currentDistance := calculateDistance(points[idx][0], points[idx][1], points[node][0], points[node][1])
+
+			// update d[i], if there is new shorter distance
+			dist[idx] = common.MinInt(dist[idx], currentDistance)
+
+			// get the minimum distance  from all the un-visted nodes
+			if nextNode == -1 || dist[idx] < dist[nextNode] {
+				nextNode = idx
+			}
+		}
+
+		edge++
+		sum += dist[nextNode]
+		node = nextNode
+	}
+
+	return sum
+}
+
+func calculateDistance(x1, y1, x2, y2 int) int {
+	return Abs(x1, x2) + Abs(y1, y2)
 }
--- graph/mst/mst_lc1584_test.go
+++ graph/mst/mst_lc1584_test.go
@@ -30,3 +30,29 @@ func Test_minCostConnectPointsPrim(t *testing.T) {
 		})
 	}
 }
+
+func Test_minCostConnectPointsPrimOptimization(t *testing.T) {
+	type args struct {
+		points [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "case 1 - side missing without full sides",
+			args: args{
+				points: [][]int{{0, 0}, {1, 1}, {1, 0}, {-1, 1}},
+			},
+			want: 4,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := minCostConnectPointsPrimOptimization(tt.args.points)
+			assert.Equal(t, tt.want, got)
+
+		})
+	}
+}
