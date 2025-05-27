d0a50df374d99fbd26c2be6274d80a18f91f0a33
brofu <brofu.talk@gmail.com>
2025-05-27 21:08:09 +0800

    backtrack. 78. Subsets

--- backtrack/backtrack_lc78.go
+++ backtrack/backtrack_lc78.go
@@ -1,5 +1,7 @@
 package backtrack
 
+import "fmt"
+
 func subsets(nums []int) [][]int {
 
 	result := [][]int{}
@@ -52,5 +54,68 @@ func subsetsV2(nums []int) [][]int {
 	}
 
 	bt([]int{}, 0)
+	return result
+}
+
+// a WRONG version
+// understand how to use `visited`
+// and how to guarantee there is NO repeat subset
+func subsetsV3(nums []int) [][]int {
+
+	result := make([][]int, 0)
+	visted := make([]bool, len(nums))
+
+	var bt func(int, []int)
+
+	bt = func(layer int, path []int) {
+
+		fmt.Println("entry", layer, path, visted)
+		//base
+		if len(path) == layer {
+			result = append(result, path)
+		}
+
+		for idx, num := range nums {
+
+			// prune
+			if visted[idx] {
+				continue
+			}
+
+			// choose
+			visted[idx] = true
+
+			bt(layer+1, append(path, num))
+
+			// cancel
+			visted[idx] = false
+		}
+
+		fmt.Println("exit", layer, path, visted)
+	}
+
+	bt(0, []int{})
+	return result
+}
+
+func subsetsV4(nums []int) [][]int {
+
+	result := make([][]int, 0)
+
+	var bt func(int, []int)
+
+	bt = func(start int, path []int) {
+
+		fmt.Println("entry:", start, result)
+		//base
+		result = append(result, path)
+
+		for idx := start; idx < len(nums); idx++ {
+			bt(idx+1, append(path, nums[idx]))
+		}
+		fmt.Println("exist:", start, result)
+	}
+
+	bt(0, []int{})
 	return result
 }
--- backtrack/backtrack_lc78_test.go
+++ backtrack/backtrack_lc78_test.go
@@ -72,3 +72,69 @@ func Test_subsetsV2(t *testing.T) {
 		})
 	}
 }
+
+func Test_subsetsV3(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]int
+	}{
+		{
+			name: "lc case 1",
+			args: args{
+				nums: []int{1, 2, 3},
+			},
+			want: [][]int{{}, {1}, {1, 2}, {1, 2, 3}, {1, 3}, {2}, {2, 3}, {3}},
+		},
+		{
+			name: "lc case 2",
+			args: args{
+				nums: []int{0},
+			},
+			want: [][]int{{}, {0}},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := subsetsV3(tt.args.nums); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("subsets() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_subsetsV4(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]int
+	}{
+		{
+			name: "lc case 1",
+			args: args{
+				nums: []int{1, 2, 3},
+			},
+			want: [][]int{{}, {1}, {1, 2}, {1, 2, 3}, {1, 3}, {2}, {2, 3}, {3}},
+		},
+		{
+			name: "lc case 2",
+			args: args{
+				nums: []int{0},
+			},
+			want: [][]int{{}, {0}},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := subsetsV4(tt.args.nums); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("subsets() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
