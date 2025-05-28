0010bf5b971ec936f68216369ae78e1d4009b86f
brofu <brofu.talk@gmail.com>
2025-05-28 21:17:34 +0800

    backtrack. 90. Subsets II. PV1. How to prune

--- backtrack/backtrack_lc90.go
+++ backtrack/backtrack_lc90.go
@@ -2,11 +2,12 @@ package backtrack
 
 import "sort"
 
-/**
+/*
+*
 KP
-	1.	How to pruning? The duplicated elements may lead to duplicate subsets
-		*	sort the numbers
-		*	skip the number if nums[i] == nums[i-1]
+ 1. How to pruning? The duplicated elements may lead to duplicate subsets
+    *	sort the numbers
+    *	skip the number if nums[i] == nums[i-1]
 */
 func subsetsWithDup(nums []int) [][]int {
 	result := [][]int{}
@@ -33,5 +34,29 @@ func subsetsWithDup(nums []int) [][]int {
 	}
 
 	bt(nums, track, 0, &result)
+	return result
+}
+
+func subsetsWithDupPV1(nums []int) [][]int {
+
+	result := make([][]int, 0)
+	sort.Ints(nums) //KP
+	var bt func(int, []int)
+	bt = func(start int, path []int) {
+
+		//base case
+		temp := make([]int, len(path)) // KP.
+		copy(temp, path)
+		result = append(result, temp)
+
+		for i := start; i < len(nums); i++ {
+			if i > start && nums[i] == nums[i-1] { //KP. i > start.
+				continue
+			}
+			bt(i+1, append(path, nums[i]))
+		}
+
+	}
+	bt(0, []int{})
 	return result
 }
--- backtrack/backtrack_lc90_test.go
+++ backtrack/backtrack_lc90_test.go
@@ -44,3 +44,43 @@ func Test_subsetsWithDup(t *testing.T) {
 		})
 	}
 }
+
+func Test_subsetsWithDupPV1(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]int
+	}{
+		{
+			name: "lc case 1",
+			args: args{
+				nums: []int{1, 2, 2},
+			},
+			want: [][]int{{}, {1}, {1, 2}, {1, 2, 2}, {2}, {2, 2}},
+		},
+		{
+			name: "lc case 2",
+			args: args{
+				nums: []int{0},
+			},
+			want: [][]int{{}, {0}},
+		},
+		{
+			name: "lc case 3",
+			args: args{
+				nums: []int{4, 4, 4, 1, 4},
+			},
+			want: [][]int{{}, {1}, {1, 4}, {1, 4, 4}, {1, 4, 4, 4}, {1, 4, 4, 4, 4}, {4}, {4, 4}, {4, 4, 4}, {4, 4, 4, 4}},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := subsetsWithDupPV1(tt.args.nums); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("subsetsWithDupPV1() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
