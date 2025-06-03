152d3d2042e84438c296e2ef27b1b1dea66e1e77
brofu <brofu.talk@gmail.com>
2025-06-03 22:31:04 +0800

    backtrack. 39. Combination Sum. PV2

--- backtrack/backtrack_lc39.go
+++ backtrack/backtrack_lc39.go
@@ -1,5 +1,43 @@
 package backtrack
 
+/*
+KP.
+1. How to control to select 1 element for multiple times?
+2. What's the base case?
+3. Compare with problem: 78, 77, and 90, 40
+
+Time Complexity
+
+1. Let's assume
+	𝑛 = number of candidates
+	𝑇 = target value
+	𝑅 = number of valid result combinations
+	minVal = min(candidates)
+
+2. Max depth of the recursive tree:
+	O(T/minVal) = O(T)
+
+3. Branching Factor
+	At each level:
+	can pick any of the 𝑛 n candidates starting from start index
+	So, branching factor ≤ 𝑛 (due to pruning and reusing only from current index onward, it's not a full n-ary tree)
+
+4. Number of Recursive Calls
+	Similar as `enumerating all **integer combinations** (with repetitions allowed) that sum to target using values from candidates`
+	So, it's around O(2^T) (the number of path need to explore)
+
+5. Cost per Recursive Call
+	Copy cost: O(T)
+
+6. Overall
+	O(T*R), R <= 2^T
+	Hence we have `O(T*2^T)`
+
+Space Complexity
+1. Recursive stack O(T), if 1 contained in the candidates
+2. Result storage: O(T * R), R is the validated result
+*/
+
 func combinationSum(candidates []int, target int) [][]int {
 	result := [][]int{}
 	track := []int{}
@@ -68,5 +106,35 @@ func combinationSumPV1(candidates []int, target int) [][]int {
 
 	bt(track, 0, 0)
 
+	return result
+}
+
+func combinationSumPV2(candidates []int, target int) [][]int {
+
+	result := [][]int{}
+
+	var bt func(start, sum int, path []int)
+
+	bt = func(start, sum int, path []int) {
+
+		//base case
+		if sum == target { // find 1 result
+			temp := make([]int, len(path))
+			copy(temp, path)
+			result = append(result, temp)
+			return
+		}
+
+		if sum > target {
+			return
+		}
+
+		for i := start; i < len(candidates); i++ {
+			bt(i, sum+candidates[i], append(path, candidates[i]))
+		}
+
+	}
+
+	bt(0, 0, []int{})
 	return result
 }
--- backtrack/backtrack_lc39_test.go
+++ backtrack/backtrack_lc39_test.go
@@ -92,3 +92,47 @@ func Test_combinationSumV2(t *testing.T) {
 		})
 	}
 }
+
+func Test_combinationSumPV2(t *testing.T) {
+	type args struct {
+		candidates []int
+		target     int
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]int
+	}{
+		{
+			name: "lc case 1",
+			args: args{
+				candidates: []int{2, 3, 6, 7},
+				target:     7,
+			},
+			want: [][]int{{2, 2, 3}, {7}},
+		},
+		{
+			name: "lc case 2",
+			args: args{
+				candidates: []int{2, 3, 5},
+				target:     8,
+			},
+			want: [][]int{{2, 2, 2, 2}, {2, 3, 3}, {3, 5}},
+		},
+		{
+			name: "lc case 3",
+			args: args{
+				candidates: []int{2},
+				target:     1,
+			},
+			want: [][]int{},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := combinationSumPV2(tt.args.candidates, tt.args.target); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("combinationSumPV2() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
