3fbcc2e9bb61503db603c8b07b514e048f86a9e6
brofu <brofu.talk@gmail.com>
2025-06-09 20:45:05 +0800

    bfs.dfs. 1254. Number of Closed Islands. dfs and bfs

--- dfs/dfs_lc1254.go
+++ dfs/dfs_lc1254.go
@@ -1,8 +1,15 @@
 package dfs
 
-/**
+/*
+*
 KP
-	1.	Similar as 200. Only need to change the islands on boarding to sea
+ 1. Similar as 200. Only need to change the islands on boarding to sea
+
+Time Complexity
+* Similar with 200
+
+Space Complexity
+* Similar with 200
 */
 func closedIsland(grid [][]int) int {
 	result := 0
@@ -47,5 +54,120 @@ func closedIsland(grid [][]int) int {
 			}
 		}
 	}
+	return result
+}
+
+func closedIslandPV1(grid [][]int) int {
+
+	result := 0
+	m, n := len(grid), len(grid[0])
+	directions := [][]int{{-1, 0}, {1, 0}, {0, -1}, {0, 1}} // up, down, left and right
+
+	var dfs func(int, int)
+
+	dfs = func(i, j int) {
+
+		//base case
+		if i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == 1 {
+			return
+		}
+
+		grid[i][j] = 1
+
+		for _, direction := range directions {
+			dfs(i+direction[0], j+direction[1])
+		}
+
+	}
+
+	for _, row := range []int{0, m - 1} {
+		for idx := range grid[row] {
+			if grid[row][idx] == 1 {
+				continue
+			}
+			dfs(row, idx)
+		}
+	}
+	for _, col := range []int{0, n - 1} {
+		for idx := 0; idx < m; idx++ {
+			if grid[idx][col] == 1 {
+				continue
+			}
+			dfs(idx, col)
+		}
+	}
+
+	for i := 1; i < m-1; i++ {
+		for j := 1; j < n-1; j++ {
+			if grid[i][j] == 1 {
+				continue
+			}
+			result++
+			dfs(i, j)
+		}
+	}
+
+	return result
+}
+
+func closedIslandBFS(grid [][]int) int {
+
+	result := 0
+	m, n := len(grid), len(grid[0])
+	directions := [][]int{{-1, 0}, {1, 0}, {0, -1}, {0, 1}} // up, down, left and right
+
+	bfs := func(i, j int) {
+
+		grid[i][j] = 1
+
+		queue := make([][]int, 1)
+		queue[0] = []int{i, j}
+
+		for len(queue) > 0 {
+			l := len(queue)
+
+			for idx := 0; idx < l; idx++ {
+				node := queue[idx]
+				for _, direction := range directions {
+					i := node[0] + direction[0]
+					j := node[1] + direction[1]
+					if i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == 1 {
+						continue
+					}
+					queue = append(queue, []int{i, j})
+					grid[i][j] = 1
+				}
+			}
+			queue = queue[l:]
+		}
+	}
+
+	for _, row := range []int{0, m - 1} {
+		for idx := range grid[row] {
+			if grid[row][idx] == 1 {
+				continue
+			}
+			bfs(row, idx)
+		}
+	}
+	for _, col := range []int{0, n - 1} {
+		for idx := 0; idx < m; idx++ {
+			if grid[idx][col] == 1 {
+				continue
+			}
+			bfs(idx, col)
+		}
+	}
+
+	for i := 1; i < m-1; i++ {
+		for j := 1; j < n-1; j++ {
+			if grid[i][j] == 1 {
+				continue
+			}
+			result++
+			bfs(i, j)
+		}
+	}
+
 	return result
 }
--- dfs/dfs_lc1254_test.go
+++ dfs/dfs_lc1254_test.go
@@ -1,6 +1,8 @@
 package dfs
 
-import "testing"
+import (
+	"testing"
+)
 
 func Test_closedIsland(t *testing.T) {
 	type args struct {
@@ -50,6 +52,151 @@ func Test_closedIsland(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := closedIsland(tt.args.grid); got != tt.want {
 				t.Errorf("closedIsland() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_closedIslandPV1(t *testing.T) {
+	type args struct {
+		grid [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "lc case 1",
+			args: args{
+				grid: [][]int{
+					{1, 1, 1, 1, 1, 1, 1, 0},
+					{1, 0, 0, 0, 0, 1, 1, 0},
+					{1, 0, 1, 0, 1, 1, 1, 0},
+					{1, 0, 0, 0, 0, 1, 0, 1},
+					{1, 1, 1, 1, 1, 1, 1, 0},
+				},
+			},
+			want: 2,
+		},
+		{
+			name: "lc case 2",
+			args: args{
+				grid: [][]int{{0, 0, 1, 0, 0}, {0, 1, 0, 1, 0}, {0, 1, 1, 1, 0}},
+			},
+			want: 1,
+		},
+		{
+			name: "lc case 3",
+			args: args{
+				grid: [][]int{{1, 1, 1, 1, 1, 1, 1},
+					{1, 0, 0, 0, 0, 0, 1},
+					{1, 0, 1, 1, 1, 0, 1},
+					{1, 0, 1, 0, 1, 0, 1},
+					{1, 0, 1, 1, 1, 0, 1},
+					{1, 0, 0, 0, 0, 0, 1},
+					{1, 1, 1, 1, 1, 1, 1},
+				},
+			},
+			want: 2,
+		},
+		{
+			name: "lc case 4",
+			args: args{
+				grid: [][]int{
+					{0, 0, 1, 1, 0, 1, 0, 0, 1, 0},
+					{1, 1, 0, 1, 1, 0, 1, 1, 1, 0},
+					{1, 0, 1, 1, 1, 0, 0, 1, 1, 0},
+					{0, 1, 1, 0, 0, 0, 0, 1, 0, 1},
+					{0, 0, 0, 0, 0, 0, 1, 1, 1, 0},
+					{0, 1, 0, 1, 0, 1, 0, 1, 1, 1},
+					{1, 0, 1, 0, 1, 1, 0, 0, 0, 1},
+					{1, 1, 1, 1, 1, 1, 0, 0, 0, 0},
+					{1, 1, 1, 0, 0, 1, 0, 1, 0, 1},
+					{1, 1, 1, 0, 1, 1, 0, 1, 1, 0},
+				},
+			},
+			want: 5,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			for _, r := range tt.args.grid {
+				t.Log(r)
+			}
+			if got := closedIslandPV1(tt.args.grid); got != tt.want {
+				t.Errorf("closedIslandPV1() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_closedIslandBFS(t *testing.T) {
+	type args struct {
+		grid [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "lc case 1",
+			args: args{
+				grid: [][]int{
+					{1, 1, 1, 1, 1, 1, 1, 0},
+					{1, 0, 0, 0, 0, 1, 1, 0},
+					{1, 0, 1, 0, 1, 1, 1, 0},
+					{1, 0, 0, 0, 0, 1, 0, 1},
+					{1, 1, 1, 1, 1, 1, 1, 0},
+				},
+			},
+			want: 2,
+		},
+		{
+			name: "lc case 2",
+			args: args{
+				grid: [][]int{{0, 0, 1, 0, 0}, {0, 1, 0, 1, 0}, {0, 1, 1, 1, 0}},
+			},
+			want: 1,
+		},
+		{
+			name: "lc case 3",
+			args: args{
+				grid: [][]int{{1, 1, 1, 1, 1, 1, 1},
+					{1, 0, 0, 0, 0, 0, 1},
+					{1, 0, 1, 1, 1, 0, 1},
+					{1, 0, 1, 0, 1, 0, 1},
+					{1, 0, 1, 1, 1, 0, 1},
+					{1, 0, 0, 0, 0, 0, 1},
+					{1, 1, 1, 1, 1, 1, 1},
+				},
+			},
+			want: 2,
+		},
+		{
+			name: "lc case 4",
+			args: args{
+				grid: [][]int{
+					{0, 0, 1, 1, 0, 1, 0, 0, 1, 0},
+					{1, 1, 0, 1, 1, 0, 1, 1, 1, 0},
+					{1, 0, 1, 1, 1, 0, 0, 1, 1, 0},
+					{0, 1, 1, 0, 0, 0, 0, 1, 0, 1},
+					{0, 0, 0, 0, 0, 0, 1, 1, 1, 0},
+					{0, 1, 0, 1, 0, 1, 0, 1, 1, 1},
+					{1, 0, 1, 0, 1, 1, 0, 0, 0, 1},
+					{1, 1, 1, 1, 1, 1, 0, 0, 0, 0},
+					{1, 1, 1, 0, 0, 1, 0, 1, 0, 1},
+					{1, 1, 1, 0, 1, 1, 0, 1, 1, 0},
+				},
+			},
+			want: 5,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := closedIslandBFS(tt.args.grid); got != tt.want {
+				t.Errorf("closedIslandBFS() = %v, want %v", got, tt.want)
 			}
 		})
 	}
