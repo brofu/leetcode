71faf21faa78e4f055b66f39c8307bfaf8643c34
brofu <brofu.talk@gmail.com>
2025-06-09 20:00:18 +0800

    dfs.bfs. 200. Number of Islands BFS

--- dfs/dfs_lc200.go
+++ dfs/dfs_lc200.go
@@ -7,7 +7,17 @@ KP
  2. 为什么每次遇到岛屿，都要用 DFS 算法把岛屿「淹了」呢？主要是为了省事，避免维护 visited 数组。 因为 dfs 函数遍历到值为 0 的位置会直接返回，所以只要把经过的位置都设置为 0，就可以起到不走回头路的作用。
 
 Time Complexity
+1. O(N+4N) = O(N) N = m*n
+2. Loop all the node outside, m*n
+3. Overall O(N)
+
+Space Complexity
+1. For DFS, Recursive stack depth: worst at `O(N)`
+2. For BFS, Queue length: worst at `O(N)`
+3. Worst case example, `m=1, n = N, and all equals 1`
+4. Use `grid` as  visited, no extra spaces
 */
+
 func numIslands(grid [][]byte) int {
 
 	result := 0
@@ -111,5 +121,49 @@ func numIslandsPV2(grid [][]byte) int {
 			dfs(i, j)
 		}
 	}
+	return result
+}
+
+func numIslandsBFS(grid [][]byte) int {
+
+	result := 0
+	m, n := len(grid), len(grid[0])
+	directions := [][]int{{-1, 0}, {+1, 0}, {0, -1}, {0, +1}}
+
+	bfs := func(i, j int) {
+
+		grid[i][j] = 0
+
+		queue := make([][]int, 1)
+		queue[0] = []int{i, j}
+
+		for len(queue) > 0 {
+			l := len(queue)
+			for idx := 0; idx < l; idx++ {
+				node := queue[idx]
+				for _, direction := range directions {
+					i := node[0] + direction[0]
+					j := node[1] + direction[1]
+					if i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == 0 {
+						continue
+					}
+					queue = append(queue, []int{i, j})
+					grid[i][j] = 0
+				}
+			}
+			queue = queue[l:]
+		}
+	}
+
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if grid[i][j] == 0 {
+				continue
+			}
+			result += 1
+			bfs(i, j)
+		}
+	}
+
 	return result
 }
--- dfs/dfs_lc200_test.go
+++ dfs/dfs_lc200_test.go
@@ -132,3 +132,47 @@ func Test_numIslandsPV2(t *testing.T) {
 		})
 	}
 }
+
+func Test_numIslandsBFS(t *testing.T) {
+	type args struct {
+		grid [][]byte
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "lc case 1",
+			args: args{
+				grid: [][]byte{
+					{1, 1, 1, 1, 0},
+					{1, 1, 0, 1, 0},
+					{1, 1, 0, 0, 0},
+					{0, 0, 0, 0, 0},
+				},
+			},
+			want: 1,
+		},
+		{
+			name: "lc case 2",
+			args: args{
+				grid: [][]byte{
+					{1, 1, 0, 0, 0},
+					{1, 1, 0, 0, 0},
+					{0, 0, 1, 0, 0},
+					{0, 0, 0, 1, 1},
+				},
+			},
+			want: 3,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Log(tt.args.grid)
+			if got := numIslandsBFS(tt.args.grid); got != tt.want {
+				t.Errorf("numIslandsBFS() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
