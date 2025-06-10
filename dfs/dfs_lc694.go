c06448b7bddc48855784c0fe3090dfffe15e6bf1
brofu <brofu.talk@gmail.com>
2025-06-10 20:57:05 +0800

    dfs. 694. Number of Distinct Islands PV1

--- dfs/dfs_lc694.go
+++ dfs/dfs_lc694.go
@@ -5,11 +5,22 @@ import (
 	"strings"
 )
 
-/**
+/*
+*
 KP.
-	1.	How to check if 2 islands are same?
-	很显然我们得想办法把二维矩阵中的「岛屿」进行转化，变成比如字符串这样的类型，然后利用 HashSet 这样的数据结构去重，最终得到不同的岛屿的个数。如果想把岛屿转化成字符串，说白了就是序列化，序列化说白了就是遍历嘛，前文 二叉树的序列化和反序列化 讲了二叉树和字符串互转，这里也是类似的。 首先，对于形状相同的岛屿，如果从同一起点出发，dfs 函数遍历的顺序肯定是一样的。需要记录撤销操作的路径
-	2.	Actually, this is `backtrack`
+ 1. How to check if 2 islands are same?
+    很显然我们得想办法把二维矩阵中的「岛屿」进行转化，变成比如字符串这样的类型，然后利用 HashSet 这样的数据结构去重，最终得到不同的岛屿的个数。如果想把岛屿转化成字符串，说白了就是序列化，序列化说白了就是遍历嘛，前文 二叉树的序列化和反序列化 讲了二叉树和字符串互转，这里也是类似的。 首先，对于形状相同的岛屿，如果从同一起点出发，dfs 函数遍历的顺序肯定是一样的。需要记录撤销操作的路径
+
+Time Complexity
+1. Recursive call times O(N+4N) = O(5N)
+2. strings.Builder.WirteString : worst case O(2*N)
+3. out side for i, j loop: O(N)
+Overall O(N)
+
+Space Complexity
+1. Recursive stack depth: worst O(N)
+2. strings.Builder worst: O(2*N) = O(N)
+3. The map depends. Should
 */
 func numDistinctIslands(grid [][]int) int {
 	directions := [][]int{{-1, 0, 1}, {1, 0, 2}, {0, -1, 3}, {0, 1, 4}} // directions and direction numbers
@@ -45,5 +56,38 @@ func numDistinctIslands(grid [][]int) int {
 			}
 		}
 	}
+	return len(islandsMap)
+}
+
+func numDistinctIslandsPV1(grid [][]int) int {
+
+	directions := [][]int{{-1, 0, 1}, {1, 0, 2}, {0, -1, 3}, {0, 1, 4}} // directions and direction numbers
+	m, n := len(grid), len(grid[0])
+
+	var dfs func(i, j int, sb *strings.Builder, dir int)
+	dfs = func(i, j int, sb *strings.Builder, dir int) {
+
+		if i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == 0 {
+			return
+		}
+		grid[i][j] = 0
+		sb.WriteString(fmt.Sprintf("%d,", dir))
+		for _, direction := range directions {
+			dfs(i+direction[0], j+direction[1], sb, direction[2])
+		}
+		sb.WriteString(fmt.Sprintf("%d,", -dir))
+	}
+
+	islandsMap := make(map[string]struct{})
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if grid[i][j] == 1 {
+				sb := &strings.Builder{}
+				dfs(i, j, sb, 5)
+				islandsMap[sb.String()] = struct{}{}
+			}
+		}
+	}
+
 	return len(islandsMap)
 }
--- dfs/dfs_lc694_test.go
+++ dfs/dfs_lc694_test.go
@@ -1,6 +1,8 @@
 package dfs
 
-import "testing"
+import (
+	"testing"
+)
 
 func Test_numDistinctIslands(t *testing.T) {
 	type args struct {
@@ -43,6 +45,52 @@ func Test_numDistinctIslands(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := numDistinctIslands(tt.args.grid); got != tt.want {
 				t.Errorf("numDistinctIslands() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_numDistinctIslandsPV1(t *testing.T) {
+	type args struct {
+		grid [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "lc case 1",
+			args: args{
+				grid: [][]int{
+					{1, 1, 0, 0, 0}, {1, 1, 0, 0, 0}, {0, 0, 0, 1, 1}, {0, 0, 0, 1, 1},
+				},
+			},
+			want: 1,
+		},
+		{
+			name: "lc case 2",
+			args: args{
+				grid: [][]int{
+					{1, 1, 0, 1, 1}, {1, 0, 0, 0, 0}, {0, 0, 0, 0, 1}, {1, 1, 0, 1, 1},
+				},
+			},
+			want: 3,
+		},
+		{
+			name: "lc case 3",
+			args: args{
+				grid: [][]int{
+					{0, 0, 1, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0}, {0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0}, {0, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0}, {1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1},
+				},
+			},
+			want: 15,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := numDistinctIslandsPV1(tt.args.grid); got != tt.want {
+				t.Errorf("numDistinctIslandsPV1() = %v, want %v", got, tt.want)
 			}
 		})
 	}
