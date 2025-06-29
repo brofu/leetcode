28903b0e1b403e8de739dc60f61440c458f18b94
brofu <brofu.talk@gmail.com>
2025-06-29 23:17:55 +0800

    dfs.bfs.695. Max Area of Island

--- dfs/dfs_lc695.go
+++ dfs/dfs_lc695.go
@@ -2,9 +2,10 @@ package dfs
 
 import "github.com/brofu/leetcode/common"
 
-/**
+/*
+*
 KP.
-	1.	Calculate the number of sub lands, similar to the post-order traverse of binary tree
+ 1. Calculate the number of sub lands, similar to the post-order traverse of binary tree
 */
 func maxAreaOfIsland(grid [][]int) int {
 	result := 0
@@ -37,5 +38,139 @@ func maxAreaOfIsland(grid [][]int) int {
 			}
 		}
 	}
+	return result
+}
+
+func maxAreaOfIslandDFS(grid [][]int) int {
+
+	result, temp := 0, 0
+	m, n := len(grid), len(grid[0])
+	directions := [][]int{{-1, 0}, {1, 0}, {0, -1}, {0, 1}} // up, down, left and right
+
+	var dfs func(int, int)
+
+	dfs = func(i, j int) {
+
+		if i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == 0 {
+			return
+		}
+
+		grid[i][j] = 0
+		temp++
+
+		for _, direction := range directions {
+			dfs(i+direction[0], j+direction[1])
+		}
+	}
+
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if grid[i][j] == 0 {
+				continue
+			}
+			temp = 0
+			dfs(i, j)
+			if result < temp {
+				result = temp
+			}
+		}
+	}
+
+	return result
+}
+
+func maxAreaOfIslandBFS(grid [][]int) int {
+
+	result, temp := 0, 0
+	directions := [][]int{{-1, 0}, {1, 0}, {0, -1}, {0, 1}} // up, down, left and right
+
+	m, n := len(grid), len(grid[0])
+
+	bfs := func(i, j int) {
+
+		grid[i][j] = 0
+		temp++
+
+		q := [][]int{{i, j}}
+
+		for len(q) > 0 {
+			l := len(q)
+			for idx := 0; idx < l; idx++ {
+				current := q[idx]
+				for _, direction := range directions {
+					i := current[0] + direction[0]
+					j := current[1] + direction[1]
+					if i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == 0 {
+						continue
+					}
+					grid[i][j] = 0
+					temp++
+					q = append(q, []int{i, j})
+				}
+			}
+			q = q[l:]
+		}
+	}
+
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if grid[i][j] == 0 {
+				continue
+			}
+			temp = 0
+			bfs(i, j)
+			if result < temp {
+				result = temp
+			}
+		}
+	}
+
+	return result
+}
+
+func maxAreaOfIslandBFSV2(grid [][]int) int {
+
+	result, temp := 0, 0
+	directions := [][]int{{-1, 0}, {1, 0}, {0, -1}, {0, 1}} // up, down, left and right
+
+	m, n := len(grid), len(grid[0])
+
+	bfs := func(i, j int) {
+
+		grid[i][j] = 0
+		temp++
+
+		q := [][]int{{i, j}}
+
+		idx := 0
+		for idx < len(q) {
+			current := q[idx]
+			for _, direction := range directions {
+				i := current[0] + direction[0]
+				j := current[1] + direction[1]
+				if i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == 0 {
+					continue
+				}
+				grid[i][j] = 0
+				temp++
+				q = append(q, []int{i, j})
+			}
+			idx++
+		}
+	}
+
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if grid[i][j] == 0 {
+				continue
+			}
+			temp = 0
+			bfs(i, j)
+			if result < temp {
+				result = temp
+			}
+		}
+	}
+
 	return result
 }
--- dfs/dfs_lc695_test.go
+++ dfs/dfs_lc695_test.go
@@ -1,6 +1,8 @@
 package dfs
 
-import "testing"
+import (
+	"testing"
+)
 
 func Test_maxAreaOfIsland(t *testing.T) {
 	type args struct {
@@ -41,6 +43,94 @@ func Test_maxAreaOfIsland(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := maxAreaOfIsland(tt.args.grid); got != tt.want {
 				t.Errorf("maxAreaOfIsland() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_maxAreaOfIslandDFS(t *testing.T) {
+	type args struct {
+		grid [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "lc case 1",
+			args: args{
+				grid: [][]int{
+					{0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+					{0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+					{0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+					{0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0},
+					{0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0},
+					{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+					{0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+					{0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0},
+				},
+			},
+			want: 6,
+		},
+		{
+			name: "lc case 2",
+			args: args{
+				grid: [][]int{
+					{0, 0, 0, 0, 0, 0, 0, 0},
+				},
+			},
+			want: 0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maxAreaOfIslandDFS(tt.args.grid); got != tt.want {
+				t.Errorf("maxAreaOfIslandDFS() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_maxAreaOfIslandBFS(t *testing.T) {
+	type args struct {
+		grid [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "lc case 1",
+			args: args{
+				grid: [][]int{
+					{0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+					{0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+					{0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+					{0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0},
+					{0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0},
+					{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+					{0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+					{0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0},
+				},
+			},
+			want: 6,
+		},
+		{
+			name: "lc case 2",
+			args: args{
+				grid: [][]int{
+					{0, 0, 0, 0, 0, 0, 0, 0},
+				},
+			},
+			want: 0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maxAreaOfIslandBFS(tt.args.grid); got != tt.want {
+				t.Errorf("maxAreaOfIslandBFS() = %v, want %v", got, tt.want)
 			}
 		})
 	}
