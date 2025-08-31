abafc215ea56f881eaddb24efd435583ee3a4af8
brofu <brofu.talk@gmail.com>
2025-08-31 14:52:50 +0800

    bfs. 994. Rotting Oranges

--- bfs/bfs_lc994.go
+++ bfs/bfs_lc994.go
@@ -71,3 +71,60 @@ func orangesRotting(grid [][]int) int {
 
 	return -1
 }
+
+/*
+Optimization:
+1. User counter to record the number of fresh.
+*/
+func orangesRottingSpaceOptimized(grid [][]int) int {
+
+	var (
+		result, freshes int
+		m, n            = len(grid), len(grid[0])
+		rottens         = [][]int{}
+		directions      = [][]int{{-1, 0}, {1, 0}, {0, 1}, {0, -1}}
+	)
+
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if grid[i][j] == 2 {
+				rottens = append(rottens, []int{i, j})
+				continue
+			}
+			if grid[i][j] == 1 {
+				freshes++
+			}
+		}
+	}
+	if freshes == 0 {
+		return result
+	}
+	if len(rottens) == 0 {
+		return -1
+	}
+
+	q := rottens
+	for len(q) > 0 {
+		l := len(q)
+		for idx := 0; idx < l; idx++ {
+			current := q[idx]
+			for _, direction := range directions {
+				x := current[0] + direction[0]
+				y := current[1] + direction[1]
+				if x < 0 || x >= m || y < 0 || y >= n || grid[x][y] == 2 || grid[x][y] == 0 {
+					continue
+				}
+				grid[x][y] = 2
+				freshes--
+				q = append(q, []int{x, y})
+			}
+		}
+		result++
+		if freshes == 0 {
+			return result
+		}
+		q = q[l:]
+	}
+
+	return -1
+}
