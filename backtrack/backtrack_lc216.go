90bca86974c849ead3491ef1386fa545d5aa89cd
brofu <brofu.talk@gmail.com>
2026-01-20 15:07:49 +0800

    backtrack. 216. Combination Sum III. type I

--- backtrack/backtrack_lc216.go
+++ backtrack/backtrack_lc216.go
@@ -27,3 +27,44 @@ func combinationSum3(k int, n int) [][]int {
 	bt([]int{}, 1, 0)
 	return result
 }
+
+/*
+
+TC:
+	1. Search node number is C(9,1) + C(9,2) + ... + C(9, k),
+	2. Upper bound of possible results is C(9, k)
+	3. With copy cost, we have C(9, k)*k
+
+SC:
+	1. recursive depth: O(k)
+*/
+func combinationSum3V1(k int, n int) [][]int {
+
+	var (
+		result [][]int
+		bt     func(int, int, []int)
+	)
+
+	bt = func(idx, sum int, track []int) {
+
+		// base case
+		if sum == n && len(track) == k { // get 1 result
+			temp := make([]int, k)
+			copy(temp, track)
+			result = append(result, temp)
+			return
+		}
+
+		if idx == 10 || sum > n || len(track) > k {
+			return
+		}
+
+		for i := idx; i < 10; i++ {
+			// choose, explore, cancel
+			bt(i+1, sum+i, append(track, i))
+		}
+	}
+
+	bt(1, 0, []int{})
+	return result
+}
--- backtrack/backtrack_lc216_test.go
+++ backtrack/backtrack_lc216_test.go
@@ -39,3 +39,39 @@ func Test_combinationSum3(t *testing.T) {
 		})
 	}
 }
+
+func Test_combinationSum3V1(t *testing.T) {
+	type args struct {
+		k int
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]int
+	}{
+		{
+			name: "lc case 1",
+			args: args{
+				k: 3,
+				n: 7,
+			},
+			want: [][]int{{1, 2, 4}},
+		},
+		{
+			name: "lc case 2",
+			args: args{
+				k: 3,
+				n: 9,
+			},
+			want: [][]int{{1, 2, 6}, {1, 3, 5}, {2, 3, 4}},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := combinationSum3V1(tt.args.k, tt.args.n); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("combinationSum3V1() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
