4117b5a49a8f4a4c55747cb54ffdeafa1ea1ad1e
brofu <brofu.talk@gmail.com>
2026-01-22 23:17:56 +0800

    dp. 354. Russian Doll Envelopes. Binary Search

--- dp/dp_lc354.go
+++ dp/dp_lc354.go
@@ -27,7 +27,6 @@ func maxEnvelopesV2(envelopes [][]int) int {
 		return envelopes[i][0] < envelopes[j][0]
 	})
 
-	fmt.Println("flag", envelopes)
 	piles := 0
 	top := make([]int, len(envelopes))
 
@@ -78,4 +77,90 @@ func maxEnvelopesDP(envelopes [][]int) int {
 	}
 
 	return result
+}
+
+/*
+
+KP:
+	1. 先对宽度 w 进行升序排序，如果遇到 w 相同的情况，则按照高度 h 降序排序；之后把所有的 h 作为一个数组，在这个数组上计算 LIS 的长度就是答案。
+	2. The DP version. Can not pass the letcode testing
+
+SC:
+	1. sort. O(N*lgN)
+	2. LIS on 2nd elements in envelops
+*/
+
+func maxEnvelopesV3(envelopes [][]int) int {
+
+	var (
+		result int
+		dp     = make([]int, len(envelopes))
+	)
+
+	for idx := range dp {
+		dp[idx] = 1
+	}
+
+	sort.Slice(envelopes, func(i, j int) bool {
+		if envelopes[i][0] == envelopes[j][0] {
+			return envelopes[i][1] > envelopes[j][1]
+		}
+		return envelopes[i][0] < envelopes[j][0]
+	})
+
+	for i := 1; i < len(envelopes); i++ {
+		for j := 0; j < i; j++ {
+			if envelopes[i][1] > envelopes[j][1] {
+				if dp[i] < dp[j]+1 {
+					dp[i] = dp[j] + 1
+				}
+			}
+		}
+	}
+	for _, temp := range dp {
+		if result < temp {
+			result = temp
+		}
+	}
+
+	return result
+}
+
+func maxEnvelopesV4(envelopes [][]int) int {
+
+	var (
+		piles int
+		top   = make([]int, len(envelopes))
+	)
+
+	sort.Slice(envelopes, func(i, j int) bool {
+		if envelopes[i][0] == envelopes[j][0] {
+			return envelopes[i][1] > envelopes[j][1]
+		}
+		return envelopes[i][0] < envelopes[j][0]
+	})
+
+	fmt.Println("flag", envelopes)
+
+	for idx, num := range envelopes {
+
+		left, right := 0, piles
+		for left < right {
+			mid := (left + right) / 2
+			if num[1] < top[mid] {
+				right = mid
+			} else if num[1] > top[mid] {
+				left = mid + 1
+			} else {
+				right = mid // get the left-most
+			}
+		}
+		if left == piles {
+			piles++
+		}
+		fmt.Println("flag", idx, left, piles)
+		top[left] = num[1]
+	}
+
+	return piles
 }
--- dp/dp_lc354_test.go
+++ dp/dp_lc354_test.go
@@ -127,3 +127,171 @@ func Test_maxEnvelopesDP(t *testing.T) {
 		})
 	}
 }
+
+func Test_maxEnvelopesV3(t *testing.T) {
+	type args struct {
+		envelopes [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "case 1",
+			args: args{
+				envelopes: [][]int{
+					{5, 4}, {6, 4}, {6, 7}, {2, 3},
+				},
+			},
+			want: 3,
+		},
+		{
+			name: "case 2",
+			args: args{
+				envelopes: [][]int{
+					{1, 1}, {1, 1}, {1, 1},
+				},
+			},
+			want: 1,
+		},
+		{
+			name: "case 3",
+			args: args{
+				envelopes: [][]int{
+					{30, 50}, {12, 2}, {3, 4}, {12, 15},
+				},
+			},
+			want: 3,
+		},
+		{
+			name: "case 4",
+			args: args{
+				envelopes: [][]int{
+					{5, 4}, {5, 2},
+				},
+			},
+			want: 1,
+		},
+		{
+			name: "case 5",
+			args: args{
+				envelopes: [][]int{
+					{5, 2}, {5, 4},
+				},
+			},
+			want: 1,
+		},
+		{
+			name: "case 6",
+			args: args{
+				envelopes: [][]int{
+					{2, 3},
+					{5, 4},
+					{6, 7},
+					{6, 4}, // same width as {6, 7}, smaller height
+					{7, 5},
+				},
+			},
+			want: 3,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maxEnvelopesV3(tt.args.envelopes); got != tt.want {
+				t.Errorf("maxEnvelopesV3() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_maxEnvelopesV4(t *testing.T) {
+	type args struct {
+		envelopes [][]int
+	}
+	tests := []struct {
+		name    string
+		enabled bool
+		args    args
+		want    int
+	}{
+		{
+			name: "case 1",
+			args: args{
+				envelopes: [][]int{
+					{5, 4}, {6, 4}, {6, 7}, {2, 3},
+				},
+			},
+			want: 3,
+		},
+		{
+			name: "case 2",
+			args: args{
+				envelopes: [][]int{
+					{1, 1}, {1, 1}, {1, 1},
+				},
+			},
+			want: 1,
+		},
+		{
+			name: "case 3",
+			args: args{
+				envelopes: [][]int{
+					{30, 50}, {12, 2}, {3, 4}, {12, 15},
+				},
+			},
+			want: 3,
+		},
+		{
+			name: "case 4",
+			args: args{
+				envelopes: [][]int{
+					{5, 4}, {5, 2},
+				},
+			},
+			want: 1,
+		},
+		{
+			name: "case 5",
+			args: args{
+				envelopes: [][]int{
+					{5, 2}, {5, 4},
+				},
+			},
+			want: 1,
+		},
+		{
+			name: "case 6",
+			args: args{
+				envelopes: [][]int{
+					{2, 3},
+					{5, 4},
+					{6, 7},
+					{6, 4}, // same width as {6, 7}, smaller height
+					{7, 5},
+				},
+			},
+			want: 3,
+		},
+		{
+			name:    "case 7",
+			enabled: true,
+			args: args{
+				envelopes: [][]int{
+					{4, 5}, {4, 6}, {6, 7}, {2, 3}, {1, 1},
+				},
+			},
+			want: 4,
+		},
+	}
+	for _, tt := range tests {
+		if !tt.enabled {
+			continue
+		}
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maxEnvelopesV4(tt.args.envelopes); got != tt.want {
+				t.Errorf("maxEnvelopesV4() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
